8fb947c445cbe260818d77f4b6bb6e2f1b1d8632
Ainun Septian <ainunseptian79@gmail.com>
2021-05-30 09:55:19 +0700

    install React Navigation set up React Navigation create Router folder

--- src/pages/GetStarted/index.js
+++ src/pages/GetStarted/index.js
@@ -3,7 +3,7 @@ import {ImageBackground, StyleSheet, Text, View} from 'react-native';
 import {ILGetStarted, ILLogo} from '../../assets';
 import {Button, Gap} from '../../components';
 
-const GetStarted = () => {
+const GetStarted = ({navigation}) => {
   return (
     <ImageBackground source={ILGetStarted} style={styles.page}>
       <View>
@@ -13,7 +13,10 @@ const GetStarted = () => {
         </Text>
       </View>
       <View>
-        <Button title="Get Started" />
+        <Button
+          title="Get Started"
+          onPress={() => navigation.navigate('Register')}
+        />
         <Gap height={16} />
         <Button type="secondary" title="Sign In" />
       </View>
