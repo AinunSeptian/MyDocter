061a309e30f5c30cd425fe41caa442c2a39ae26c
Ainun Septian <ainunseptian79@gmail.com>
2021-06-13 10:57:36 +0700

    adding style onFocus & onBlur in Form

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import {Button, Gap, Header, Input} from '../../components';
 import {colors} from '../../utils';
 
@@ -7,19 +7,22 @@ const Register = ({navigation}) => {
   return (
     <View style={styles.page}>
       <Header onPress={() => navigation.goBack()} title="Daftar Akun" />
+
       <View style={styles.content}>
-        <Input label="Full Name" />
-        <Gap height={24} />
-        <Input label="Pekerjaan" />
-        <Gap height={24} />
-        <Input label="Email" />
-        <Gap height={24} />
-        <Input label="Password" />
-        <Gap height={40} />
-        <Button
-          title="Continue"
-          onPress={() => navigation.navigate('UploadPhoto')}
-        />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Input label="Full Name" />
+          <Gap height={24} />
+          <Input label="Pekerjaan" />
+          <Gap height={24} />
+          <Input label="Email" />
+          <Gap height={24} />
+          <Input label="Password" />
+          <Gap height={40} />
+          <Button
+            title="Continue"
+            onPress={() => navigation.navigate('UploadPhoto')}
+          />
+        </ScrollView>
       </View>
     </View>
   );
